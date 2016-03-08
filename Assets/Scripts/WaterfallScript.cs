49f7256035009a2a897894bc0c132c1eefea2f16
dubesinhower <dubesinhower@gmail.com>
2016-03-08 10:20:45 -0500

    First commit

---
+++ Assets/Scripts/WaterfallScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterfallScript : MonoBehaviour
+{
+    public int NumberOfTilesWide;
+
+    private void Awake()
+    {
+        
+    }
+
+
+	// Use this for initialization
+    private void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    private void Update () {
+	
+	}
+}
