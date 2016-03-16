1968631a0cc5aefb90b479ce93d30ee778c3facc
dubesinhower <dubesinhower@gmail.com>
2016-03-16 01:31:15 -0400

    Implementing tile map
    https://www.youtube.com/playlist?list=PLbghT7MmckI4_VM5q3va043FgAwRim6yX

---
+++ Assets/Controllers/WorldController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldController : MonoBehaviour {
+
+    public Sprite floorSprite;
+
+    World world;
+	// Use this for initialization
+	void Start () {
+        world = new World();        
+
+        for (int x = 0; x < world.Width; x++)
+        {
+            for (int y = 0; y < world.Height; y++)
+            {
+                Tile tile_data = world.GetTileAt(x, y);
+                
+                GameObject tile_go = new GameObject();
+                tile_go.name = "Tile_" + x + "_" + y;
+                tile_go.transform.position = new Vector3(tile_data.X, tile_data.Y, 0);
+
+                tile_go.AddComponent<SpriteRenderer>();
+
+                //replace with events https://www.youtube.com/watch?v=DWtbNWMrOTM&list=PLbghT7MmckI4_VM5q3va043FgAwRim6yX&index=3
+                tile_data.RegisterTileTypeChangedCallback((tile) => { OnTileTypeChanged(tile, tile_go); });
+            }
+        }
+
+        world.RandomizeTiles();
+    }
+
+    float randomizeTileTimer = 2f;
+	
+	// Update is called once per frame
+	void Update () {
+        randomizeTileTimer -= Time.deltaTime;
+
+        if(randomizeTileTimer < 0)
+        {
+            world.RandomizeTiles();
+            randomizeTileTimer = 2f;
+        }
+	}
+
+    void OnTileTypeChanged(Tile tile_data, GameObject tile_go)
+    {
+        SpriteRenderer tile_sr = tile_go.GetComponent<SpriteRenderer>();
+
+        if (tile_data.Type == Tile.TileType.Ground)
+        {
+            tile_sr.sprite = floorSprite;
+        }
+        else if(tile_data.Type == Tile.TileType.Empty)
+        {
+            tile_sr.sprite = null;
+        }
+        else
+        {
+            Debug.LogError("OnTileTypeChanged - Unrecognized tile type.");
+        }
+    }
+}
---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Tile  {
+    public enum TileType { Empty, Ground }
+
+    TileType type = TileType.Empty;
+
+    Action<Tile> tileTypeChangedCallback;
+
+    public TileType Type
+    {
+        get
+        {
+            return type;
+        }
+
+        set
+        {
+            TileType oldType = type;
+            type = value;
+            if(tileTypeChangedCallback != null && oldType != type)
+                tileTypeChangedCallback(this);
+        }
+    }
+
+    World world;
+    int x;
+    public int X
+    {
+        get
+        {
+            return x;
+        }
+    }
+    int y;
+    public int Y
+    {
+        get
+        {
+            return y;
+        }
+    }
+
+    public Tile ( World world, int x, int y)
+    {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+    }
+
+    // convert to event https://www.youtube.com/watch?v=DWtbNWMrOTM&list=PLbghT7MmckI4_VM5q3va043FgAwRim6yX&index=3
+    public void RegisterTileTypeChangedCallback(Action<Tile> callback)
+    {
+        tileTypeChangedCallback += callback;
+    }
+
+    public void UnregisterTileTypeChangedCallback(Action<Tile> callback)
+    {
+        tileTypeChangedCallback -= callback;
+    }
+}
