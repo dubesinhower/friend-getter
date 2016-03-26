228093109661146951d1021c8024e7199ef6b7ed
dubesinhower <dubesinhower@gmail.com>
2016-03-26 02:35:34 -0400

    Implement Tiled2Unity workflow
    Started using Tiled to create platformer maps, using custom prefab
    importer rules for one way platforms

---
+++ Unity/Assets/Scripts/Editor/CustomPrefabImporter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Example custom importer:
+[Tiled2Unity.CustomTiledImporter]
+class CustomPrefabImporter : Tiled2Unity.ICustomTiledImporter
+{
+    public void HandleCustomProperties(GameObject gameObject,
+        IDictionary<string, string> customProperties)
+    {
+        Debug.Log("Handle custom properties from Tiled map");
+        if (customProperties.ContainsKey("Handler"))
+        {
+            if (customProperties["Handler"] == "OneWayPlatform")
+            {
+                // Get array of one way platform box colliders
+                var boxColliders = gameObject.GetComponentsInChildren<BoxCollider2D>();
+
+                // Add platform effector to each box colliders' game object
+                foreach (var collider in boxColliders)
+                {
+                    var platformEffector = collider.gameObject.AddComponent<PlatformEffector2D>();
+                    platformEffector.surfaceArc = 178;
+                    platformEffector.useOneWay = true;
+                    collider.usedByEffector = true;
+                }
+            }
+        }
+    }
+
+    public void CustomizePrefab(GameObject prefab)
+    {
+        Debug.Log("Customize prefab");
+    }
+}
